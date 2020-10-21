dac64b7a4d9b5a522073770c9e02c558c7d745be
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-21 13:06:16 +0300

    modified script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
 function myFunction() {
     var Disease = document.getElementById("Disease").val;
-    if(Group-A) {
-        alert("covid-19");
+    if (Disease == "Malaria"){
+        confirm("fever");
+        confirm("fatigue");
+        confirm("body ache");
+        confirm("head ache");
+        confirm("cough");
+        confirm("shivering");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number>4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
+    } else if (Disease = "Covid-19"){
+        confirm("Aches and Pain");
+        confirm("Sore throat");
+        confirm("Diarrhea");
+        confirm("headache");
+        confirm("Loss of taste or smell");
+        confirm("Rush on the skin or discolouration of fingers and toes");
+        var number = parseInt(prompt("How many did you check as ok?"));
+        if (number>4){
+            alert("Visit nearest health facilty")
+        } else if(number < 4){
+            alert("Get some rest")
+        }
+        
     }
+
+    
 } 
